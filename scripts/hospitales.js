2dec56d036d525e22e5ac1325ad3662c06e1490f
planiflab <58193669+planiflab@users.noreply.github.com>
2020-06-08 09:06:31 -0300

    Add files via upload

---
+++ scripts/hospitales.js
@@ -0,0 +1,8 @@
+var hospitales ={
+"type": "FeatureCollection",
+"name": "hospitales",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NOMBRE": "LENCINAS", "DPTO": "Godoy Cruz", "DISTRITO": "V.Hipodromo", "DIRECCION": "Talcahuano S\/N", "TELEFONO": "4272600 \/ 4272773", "COMPLEJIDA": "II MONOVALENTE", "N_CAMAS": "132", "JERRAQUIA": "\/VI\/", "ESPECIALID": "ENF.END\/EP", "SERVICIO": "INTER. Y CONS. EX", "RADIO_INF": "24000", "CLASE": "Hospital", "RESPONSABL": "Lic. Luis Ten Huver" }, "geometry": { "type": "Point", "coordinates": [ -68.870817296300288, -32.903574896312243 ] } }
+]
+}
